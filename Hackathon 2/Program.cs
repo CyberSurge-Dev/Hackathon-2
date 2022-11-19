dc9bd7813321354e830172214b57c8d360030675
CreativeUsernam <zhoover.nc@gmail.com>
2022-11-19 16:01:19 -0500

    added all the things

--- Hackathon 2/Program.cs
+++ Hackathon 2/Program.cs
@@ -8,7 +8,6 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddRazorPages();
 builder.Services.AddServerSideBlazor();
 builder.Services.AddSingleton<WeatherForecastService>();
-
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
